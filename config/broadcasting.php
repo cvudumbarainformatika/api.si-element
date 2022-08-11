c2552fc196ca923cd61e4b55b2ed01f088f056b0
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2022-08-12 05:00:19 +0700

    broadcast

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -41,8 +41,8 @@ return [
                 'host' => env('PUSHER_HOST'),
                 'port' => env('PUSHER_PORT'),
                 'scheme' => env('PUSHER_SCHEME'),
-                'debug'=>true,
-                'useTLS'=>true,
+                // 'debug'=>true,
+                // 'useTLS'=>true,
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
