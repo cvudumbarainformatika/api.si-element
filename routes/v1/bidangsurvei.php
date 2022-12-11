e6dad8c30f67f9a86e10685dbd90d2715a8e64a3
farhandardiri <faran.f4124n@gmail.com>
2022-12-11 23:14:58 +0700

    edit url bidang survei

--- routes/v1/bidangsurvei.php
+++ routes/v1/bidangsurvei.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'middleware' => 'jwt.verify',
-    'prefix' => 'master'
+    'prefix' => 'bidangsurvei'
 ], function () {
     Route::get('/all', [BidangSurveiController::class, 'index']);
     Route::post('/store', [BidangSurveiController::class, 'store']);
